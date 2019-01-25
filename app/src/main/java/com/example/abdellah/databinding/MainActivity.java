d7a3af504777eac7251e4a7eafe852347dcce1ce
abdellah <selassi.abdellah@gmail.com>
2019-01-25 11:18:25 +0100

    sample with ObservableField

---
+++ app/src/main/java/com/example/abdellah/databinding/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.abdellah.databinding;
+
+import android.databinding.BaseObservable;
+import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
+import android.databinding.ObservableField;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import com.example.abdellah.databinding.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static class Increment {
+        public final ObservableField<String> inc = new ObservableField<>();
+    }
+
+    public static class MyHandlers {
+
+        public void onClickIncrement(Increment increment) {
+            increment.inc.set(Double.toString(Math.random()));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Increment increment = new Increment();
+        MyHandlers myHandlers = new MyHandlers();
+        activityMainBinding.setIncrement(increment);
+        activityMainBinding.setHandler(myHandlers);
+    }
+}
