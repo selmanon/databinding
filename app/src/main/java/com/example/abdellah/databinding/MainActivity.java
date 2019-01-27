27c4ad19ce0c8bc40f2f2db588eb7e931fa537d1
abdellah <selassi.abdellah@gmail.com>
2019-01-27 20:52:52 +0100

    sample with BindingAdapter

--- app/src/main/java/com/example/abdellah/databinding/MainActivity.java
+++ app/src/main/java/com/example/abdellah/databinding/MainActivity.java
@@ -1,35 +1,33 @@
 package com.example.abdellah.databinding;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableField;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.abdellah.databinding.databinding.ActivityMainBinding;
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public static class Increment {
-        public final ObservableField<String> inc = new ObservableField<>();
+    public static class Profile {
+        public String profileAvatar = "http://i.imgur.com/DvpvklR.png";
     }
 
-    public static class MyHandlers {
-
-        public void onClickIncrement(Increment increment) {
-            increment.inc.set(Double.toString(Math.random()));
-        }
+    @BindingAdapter(value = {"imageUrl", "error"}, requireAll = false)
+    public static void loadImage(ImageView view, String url, String error) {
+        Picasso.get().load(url).error(R.drawable.ic_launcher_foreground).into(view);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Profile profile = new Profile();
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Increment increment = new Increment();
-        MyHandlers myHandlers = new MyHandlers();
-        activityMainBinding.setIncrement(increment);
-        activityMainBinding.setHandler(myHandlers);
+        activityMainBinding.setProfile(profile);
     }
 }
