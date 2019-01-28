d57161768c284429e23f596d7a95657acc61235a
abdellah <selassi.abdellah@gmail.com>
2019-01-28 22:54:59 +0100

    lill clean

--- app/src/main/java/com/example/abdellah/databinding/MainActivity.java
+++ app/src/main/java/com/example/abdellah/databinding/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.abdellah.databinding;
 
+import android.databinding.BaseObservable;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.abdellah.databinding.databinding.ActivityMainBinding;
 import com.squareup.picasso.Picasso;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickHandler {
+
+    ActivityMainBinding activityMainBinding;
+    Profile profile;
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "click!",Toast.LENGTH_LONG).show();
+        profile.profileAvatar = "http://i.imgur.com/DvpvklR.png";
+        activityMainBinding.setProfile(profile);
+    }
 
 
-    public static class Profile {
-        public String profileAvatar = "http://i.imgur.com/DvpvklR.png";
+    public static class Profile extends BaseObservable {
+        public String profileAvatar = "https://cdn.radiofrance.fr/s3/cruiser-production/2018/09/c37f1bf6-b36e-4f5a-a866-b46b686c38dc/838_picasso.jpg";
     }
 
     @BindingAdapter(value = {"imageUrl", "error"}, requireAll = false)
@@ -25,8 +38,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Profile profile = new Profile();
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        profile = new Profile();
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setProfile(profile);
+        activityMainBinding.setClickHandler(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
